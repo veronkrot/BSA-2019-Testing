91d183283d18ca5a06ede748533ad3d78f3be67c
taras d <tarass.dubyk@gmail.com>
2019-07-18 00:03:14 +0300

    init

---
+++ src/CartParser.test.js
@@ -0,0 +1,15 @@
+import CartParser from './CartParser';
+
+let parser;
+
+beforeEach(() => {
+	parser = new CartParser();
+});
+
+describe('CartParser - unit tests', () => {
+	// Add your unit tests here.
+});
+
+describe('CartParser - integration test', () => {
+	// Add your integration test here.
+});
